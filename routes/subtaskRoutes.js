3e6d8c170120c3060e4d7a53498fa9759dccc7b2
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-31 10:03:16 +0545

    Update subtaskRoutes.js

--- routes/subtaskRoutes.js
+++ routes/subtaskRoutes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { addSubtask } from "../controllers/subtaskController.js";
+import { addSubtask, deleteSubtask, getSubtaskDetails, toggleSubtaskStatus, updateSubtask } from "../controllers/subtaskController.js";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 
 const router = Router();
 
-router.post('/add/:taskId', addSubtask);
+router.get('/api/details/:id', isAuthenticated, getSubtaskDetails)
+
+router.post('/add/:taskId', isAuthenticated, addSubtask);
+
+router.patch('/update/:id/toggle-status', isAuthenticated, toggleSubtaskStatus);
+
+router.delete('/delete/:id', isAuthenticated, deleteSubtask);
+
+router.patch('/update/:id', isAuthenticated, updateSubtask);
 
 export const subtaskRoutes = router;
