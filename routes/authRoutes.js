0b0ea56555838c4b07e67cf1050af40a0d6961ff
AkkalDhami <dhamiakkal21@gmail.com>
2025-08-02 09:38:55 +0545

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,36 +1,6 @@
 import { Router } from "express";
 import { getLogin, getRegister, postLogin, postRegister, getProfile, logout, getVerifyEmail, getResendVerificationLink, getVerifyEmailToken, getEditProfile, postEditProfile, getChangePasswordPage, postChangePassword, getGoogleLoginPage, getGoogleLoginCallbackPage, getResetPasswordPage, postForgetPassword, getResetPasswordTokenPage, postResetPassword, getGithubLoginPage, getGithubLoginCallbackPage, getSetPasswordPage, postSetPassword } from "../controllers/authController.js";
 
-import multer from "multer";
-import path from "path";
-
-const uploadPath = path.join(import.meta.dirname, '..', 'public', 'uploads');
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadPath)
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop())
-    }
-})
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
-        cb(null, true);
-    } else {
-        cb(null, 'Only .jpg, .jpeg and .png format allowed!', false);
-    }
-}
-
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
-});
-
 const router = Router();
 
 router.get('/register', getRegister);
@@ -57,7 +27,7 @@ router.route('/github').get(getGithubLoginPage);
 router.route('/github/callback').get(getGithubLoginCallbackPage);
 
 
-router.post('/edit-profile/', upload.single('avatarUrl'), postEditProfile);
+router.post('/edit-profile/', postEditProfile);
 router.post('/change-password', postChangePassword);
 router.post('/login', postLogin);
 router.post('/register', postRegister);
