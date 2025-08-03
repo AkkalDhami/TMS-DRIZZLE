3fb5cebbdc08f636b50cf7a4b0b2894ed70bf1ba
AkkalDhami <dhamiakkal21@gmail.com>
2025-08-03 09:06:20 +0545

    update toast notification

--- public/javascripts/utils/toast.js
+++ public/javascripts/utils/toast.js
@@ -3,17 +3,30 @@ function removeToast(toast) {
     if (toast.timeoutId) clearTimeout(toast.timeoutId);
     setTimeout(() => toast.remove(), 500);
 }
+
+function getIcon(type) {
+    const opt = {
+        success: 'ri-checkbox-circle-fill',
+        error: 'ri-error-warning-fill',
+        warn: 'fa-solid fa-triangle-exclamation',
+        alarm: 'fas fa-bell',
+    }
+    return opt[type];
+}
+
 export function showToast(message, type) {
     const toast = document.createElement("div");
     toast.className = `toast z-50 backdrop-blur-md bg-card ${type}`;
     toast.innerHTML = `<div class="column text-dark">
                        
-            <i class="text-dark ${type === 'success' ? 'ri-checkbox-circle-fill' : type === 'error' ? 'ri-error-warning-fill' : 'fa-solid fa-triangle-exclamation'}"></i>
+            <i class="text-dark ${getIcon(type)}"></i>
                          <span>${message}</span>
                       </div>
                       <i id="removeTOast" class="fa-solid fa-xmark text-gray cursor-pointer"></i>`;
     document.querySelector("#toastContainer").appendChild(toast);
-    toast.timeoutId = setTimeout(() => removeToast(toast), 4000);
+    if (type !== 'alarm') {
+        toast.timeoutId = setTimeout(() => removeToast(toast), 4000);
+    }
 
     // remove toast click on xmark
     toast.querySelector("#removeTOast").addEventListener("click", () => removeToast(toast));
